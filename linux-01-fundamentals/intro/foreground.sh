feb9515df244bf06b64c999a442dbb9987755ac9
André Lademann <alademann@thinkport.digital>
2024-12-06 14:03:11 +0100

    fix(foreground.sh): remove command hiding to display all commands in terminal fix(foreground.sh): correct echo command to include -e flag for escape sequences

--- linux-01-fundamentals/intro/foreground.sh
+++ linux-01-fundamentals/intro/foreground.sh
@@ -1,8 +1,4 @@
-# Hide commands from displaying on the terminal only showing the output
-set +x
-
-# Green text
 echo -e "\n\033[92mInstalling scenario …\033[0m"
 while [ ! -f /tmp/finished ]; do sleep 1; done
-echo "\n\033[92mScenario installed!\033[0m"
+echo -e "\n\033[92mScenario installed!\033[0m\n"
 /usr/games/cowsay -f tux "Hello Linux beginner!"
