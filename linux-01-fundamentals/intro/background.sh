ceb6df9c8b428efbad2d42a2490bc676ef70a6e2
André Lademann <alademann@thinkport.digital>
2024-12-06 13:55:16 +0100

    fix(background.sh): add 'set -e' to terminate script on errors to prevent continuation on failure
    style(foreground.sh): change 'set -x' to 'set +x' to hide command execution details and only show output

--- linux-01-fundamentals/intro/background.sh
+++ linux-01-fundamentals/intro/background.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # Install dependencies
 apt update
 apt -y install cowsay
--- linux-01-fundamentals/intro/foreground.sh
+++ linux-01-fundamentals/intro/foreground.sh
@@ -1,4 +1,6 @@
-set -x
+# Hide commands from displaying on the terminal only showing the output
+set +x
+
 echo "Installing scenario: $SCENARIO"
 while [ ! -f /tmp/finished ]; do sleep 1; done
 /usr/games/cowsay -f tux "Hello Linux beginner!"
