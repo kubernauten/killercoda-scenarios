2ffed8b4bbd65f5df6a55b9b03fe2729a47d72f5
André Lademann <alademann@thinkport.digital>
2024-12-06 13:59:24 +0100

    style(foreground.sh): remove variable from echo statement for cleaner output style(foreground.sh): add newline for better readability after loop

--- linux-01-fundamentals/intro/foreground.sh
+++ linux-01-fundamentals/intro/foreground.sh
@@ -1,6 +1,7 @@
 # Hide commands from displaying on the terminal only showing the output
 set +x
 
-echo "Installing scenario: $SCENARIO"
+echo "Installing scenario …"
 while [ ! -f /tmp/finished ]; do sleep 1; done
+echo ""
 /usr/games/cowsay -f tux "Hello Linux beginner!"
